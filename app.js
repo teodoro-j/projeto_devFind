f695303799314cb9ebcc7a7677f8c016d196a492
teodorojo <joao.alfredoteodoro@gmail.com>
2020-08-14 20:21:06 -0300

    adicionado arquivo e conexão db

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
+const db = require('./db/connection');
 
 const PORT = 3000;
 
 app.listen(PORT, function() {
     console.log(`O Express esta rodando na porta ${PORT}`);
 });
+
+db
+  .authenticate()
+  .then(() => {
+      console.log("Conectou ao banco com sucesso");
+  })
+  .catch(err => {
+      console.log("Ocorreu um erro ao se conectar", err);
+  });
 
 app.get('/', (req,res) => {
     res.send("Está funcionando");
---
+++ db/connection.js
@@ -0,0 +1,9 @@
+const Sequelise = require('sequelize');
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './db/app.db'
+});
+
+module.exports = sequelize;
